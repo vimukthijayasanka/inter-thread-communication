76fd2705758b197953ea7dbeb7123212e2e39fa4
Vimukthi Jayasanka <jayasanka1997@gmail.com>
2025-03-12 14:03:19 +0530

    finalize-get-request

--- src/main/java/lk/ijse/dep13/interthreadcommunication2/ObtainConnectionServlet.java
+++ src/main/java/lk/ijse/dep13/interthreadcommunication2/ObtainConnectionServlet.java
@@ -5,13 +5,21 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lk.ijse.dep13.interthreadcommunication2.db.MyCP;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet (name = "obtain-connection-servlet",urlPatterns = "/connections/")
 public class ObtainConnectionServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("ObtainConnectionServlet");
+        MyCP connectionPool = (MyCP) getServletContext().getAttribute("datasource");
+        MyCP.ConnectionWrapper cWrapper = connectionPool.getConnection();
+
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.printf("<h1>ID: %s</h1>", cWrapper.id());
+        out.printf("<h1>Connection Ref: %s</h1>", cWrapper.connection());
     }
 }
