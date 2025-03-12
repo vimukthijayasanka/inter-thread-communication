00df44bae1ca6b3439236ef5d0820a5aeb3f405c
Vimukthi Jayasanka <jayasanka1997@gmail.com>
2025-03-12 14:06:37 +0530

    finalize-delete-request

--- src/main/java/lk/ijse/dep13/interthreadcommunication2/ReleaseConnectionServlet.java
+++ src/main/java/lk/ijse/dep13/interthreadcommunication2/ReleaseConnectionServlet.java
@@ -5,13 +5,48 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lk.ijse.dep13.interthreadcommunication2.db.MyCP;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @WebServlet (name="release-connection-servlet", urlPatterns = "/connections/*")
 public class ReleaseConnectionServlet extends HttpServlet {
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("Deleting connection");
+        if (req.getPathInfo() == null || req.getPathInfo().equals("/")) {
+            releaseAllConnections(resp);
+        }else {
+            Matcher matcher = Pattern.compile("^/(?<id>[^/]+)/?(?<rest>.*)$").matcher(req.getPathInfo());
+            if (matcher.find()){
+                String id = matcher.group("id");
+                String rest = matcher.group("rest");
+                if (rest.isBlank()){
+                    releaseConnection(id, resp);
+                } else {
+                    resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+                }
+            }
+        }
+    }
+
+    private void releaseConnection(String id, HttpServletResponse resp) throws IOException {
+        MyCP connectionPool = (MyCP) getServletContext().getAttribute("datasource");
+        connectionPool.releaseConnection(Integer.valueOf(id.trim()));
+
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>Connection id: " + id + "released</h1>");
+    }
+
+    private void releaseAllConnections(HttpServletResponse resp) throws IOException {
+        MyCP connectionPool = (MyCP) getServletContext().getAttribute("datasource");
+        connectionPool.releaseAllConnections();
+
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>All Connection released</h1>");
     }
 }
