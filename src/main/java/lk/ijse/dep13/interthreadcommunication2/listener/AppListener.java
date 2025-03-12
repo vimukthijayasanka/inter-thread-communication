bdc203b6ccabd17682ba40c3033453401eb5df0e
Vimukthi Jayasanka <jayasanka1997@gmail.com>
2025-03-12 14:01:08 +0530

    app-listener-impl

--- src/main/java/lk/ijse/dep13/interthreadcommunication2/listener/AppListener.java
+++ src/main/java/lk/ijse/dep13/interthreadcommunication2/listener/AppListener.java
@@ -4,12 +4,19 @@ import jakarta.servlet.ServletContainerInitializer;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.ServletException;
+import lk.ijse.dep13.interthreadcommunication2.db.MyCP;
 
+import java.io.IOException;
 import java.util.Set;
 
 public class AppListener implements ServletContainerInitializer {
     @Override
     public void onStartup(Set<Class<?>> set, ServletContext servletContext) throws ServletException {
-
+        try {
+            MyCP myCP = new MyCP();
+            servletContext.setAttribute("myCP", myCP);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
